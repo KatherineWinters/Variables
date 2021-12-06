b80d23713a0f8b9b9d7882e28910ee984319535e
Katherine Winters <goble.katherine@gmail.com>
2021-12-05 18:45:51 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string accountType = "Checking";
+            int accountNumber = 5645;
+            char ownerLastInitial = 'W';
+            bool hasSavings = true;
+            double yearsOpen = 2.5;
+            decimal balance = 2585.36M;
+
+            Console.WriteLine($"Account: {accountType}\n" +
+                $"Account Number: {accountNumber}\n" +
+                $"Last Initial: {ownerLastInitial}\n" +
+                $"Savings: {hasSavings}\n" +
+                $"Years account has been open: {yearsOpen}\n" +
+                $"Account Balance: {balance}\n");
+        }
+    }
+}
